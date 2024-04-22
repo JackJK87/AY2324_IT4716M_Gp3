ccd56479fe3e754bdbf9b2aeece204821eaf81b3
Ellas54 <230101053@stu.vtc.edu.hk>
2024-04-23 01:53:30 +0800

    op

---
+++ restaurant/Assets/cs/cubebutton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubebutton : MonoBehaviour
+{
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log("run");
+
+    }
+
+}
